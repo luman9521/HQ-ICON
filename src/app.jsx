cd9d4ca6665fc15f965cb98a73c2c3f899445ef9
luman9521 <136189993+luman9521@users.noreply.github.com>
2024-06-11 13:24:22 +0800

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -81,7 +81,7 @@ class App extends Component {
             <div className="app">
                 <header>
                     <div className="center">
-                        <div className="logo">HQ ICON</div>
+                        <div className="logo">IOS图标(icon)</div>
                         <div className="description">从 App Store 获取高清应用图标</div>
                         <div className="options">
                             {entityMaps.map(option => this.renderOption(option.key, option.value, option.text))}
@@ -114,7 +114,7 @@ class App extends Component {
                 <main className="results">
                     {results.map((result) => (<Result key={result.trackId} data={result} cut={cut} resolution={resolution} format={format} />))}
                 </main>
-                <footer className="footer">Copyrights © 2023 - <a className="footer-msg" href='https://github.com/YuKongA'>YuKongA</a></footer>
+                <footer className="footer">轻松获取iosAPP图标！</footer>
             </div>
         );
     }
